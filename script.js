851c5f8fd90545affc63c73f2e2c1d7c7d65182c
unknown <delfinmur@gmail.com>
2024-09-20 18:43:54 +0300

    progress bar animation added

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 
+const progress = document.querySelector(".progress-bars-wrapper");
+const progressBarPercents = [90, 75, 70, 70, 65, 50];
+
 window.addEventListener("scroll", () => {
   if (window.pageYOffset >= navbarOffsetTop) {
     navbar.classList.add("sticky");
@@ -11,6 +14,7 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("sticky");
   }
 
+  // select nav section
   sections.forEach((section, i) => {
     if (window.pageYOffset >= section.offsetTop - 15) {
       navbarLinks.forEach((navbarLink) => {
@@ -20,4 +24,13 @@ window.addEventListener("scroll", () => {
       navbarLinks[i].classList.add("change");
     }
   });
+
+  // progress bar
+  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+    document.querySelectorAll(".progress-percent").forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`;
+      el.previousElementSibling.firstElementChild.textContent =
+        progressBarPercents[i];
+    });
+  }
 });
